0c41b8ab70dbc59fbe406a533100dca7b060ce74
Mike Schulze <mike.schulze@live.com>
2016-02-26 19:47:54 -0700

    Missed a file.

---
+++ HexHelper.JsonApi/HexApi/IMessage.cs
@@ -0,0 +1,39 @@
+﻿namespace HexHelper.JsonApi.HexApi
+{
+    public enum MessageType
+    {
+        Unknown,
+        Talents,
+        Inventory,
+        Collection,
+        Login,
+        Logout,
+        SaveDeck,
+        DraftPack,
+        DraftCardPicked,
+        GameStarted,
+        GameEnded,
+        PlayedUpdated,
+        CardUpdated,
+        PlayerUpdated
+    }
+
+    public interface IMessage
+    {
+        MessageType Type { get; }
+        bool LogToFile { get;  }
+    };
+
+    public class GenericMessage : IMessage
+    {
+        public GenericMessage( MessageType aType, bool aLogToFile = false )
+        {
+            Type = aType;
+            LogToFile = aLogToFile;
+        }
+
+        public bool LogToFile { get; private set; } = false;
+
+        public MessageType Type { get; private set; } = MessageType.Collection;
+    }
+}
