7779f6d3ade2c21b08d5c922548734556edaed28
Mike Schulze <mike.schulze@live.com>
2016-08-05 19:40:39 -0700

    Added icon.

--- HexHelper.WinDesktop/Properties/VersionInfo.cs
+++ HexHelper.WinDesktop/Properties/VersionInfo.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyVersion("1.0.0.25")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.0.25")]
+[assembly: System.Reflection.AssemblyVersion("1.0.0.26")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.26")]
 
 
